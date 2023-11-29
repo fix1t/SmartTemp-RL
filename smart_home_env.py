205d6058be02942869efada240453806d9e6183f
Fix1t <gbiel1117@gmail.com>
2023-11-29 17:38:27 +0100

    added random event handler

--- smart_home_env.py
+++ smart_home_env.py
@@ -9,27 +9,27 @@ from smart_home_config import CONFIG
 class SmartHomeTempControlEnv(gym.Env):
     metadata = {'render.modes': ['console']}
 
-    def __init__(self):
+    def __init__(self, config=CONFIG):
         super(SmartHomeTempControlEnv, self).__init__()
         self.action_space = spaces.Discrete(3)  # 0: Heat Up, 1: Cool Down, 2: Do Nothing
         self.observation_space = spaces.Box(low=np.array([0]), high=np.array([50]), dtype=np.float32) # Current temperature
 
         # Define initial conditions
-        self.user_preference = CONFIG['user_preference']
-        self.current_temperature = CONFIG['starting_temperature']
-        self.outside_temperature = CONFIG['outside_temperature']
-        self.insulation_factor = CONFIG['insulation_quality']
-        self.time_factor = CONFIG['time_factor']
-        self.heater_at_max = CONFIG['heater_at_max']
-        self.cooler_at_max = CONFIG['cooler_at_max']
-        self.hvac_efficiency = CONFIG['hvac_efficiency']
-        self.meter_step = CONFIG['meter_step']
+        self.user_preference = config['user_preference']
+        self.current_temperature = config['starting_temperature']
+        self.outside_temperature = config['outside_temperature']
+        self.insulation_factor = config['insulation_quality']
+        self.time_factor = config['time_factor']
+        self.heater_at_max = config['heater_at_max']
+        self.cooler_at_max = config['cooler_at_max']
+        self.hvac_efficiency = config['hvac_efficiency']
+        self.meter_step = config['meter_step']
 
         # Define weekly schedule
-        self.schedule = CONFIG['weekly_schedule']
-        self.random_event = CONFIG['random_event']
-        self.random_event_chance = CONFIG['random_event_chance']
-        self.random_event_max_duration = CONFIG['random_event_max_duration']
+        self.weekly_schedule = config['weekly_schedule']
+        self.random_event = config['random_event']
+        self.random_event_chance = config['random_event_chance']
+        self.random_event_max_duration = config['random_event_max_duration']
         self.people_presence = {
             "father": True,
             "mother": True,
@@ -39,6 +39,9 @@ class SmartHomeTempControlEnv(gym.Env):
         # Initial time 1/1/2020 00:00
         self.current_time = datetime(2020, 1, 1, 0, 0)
         self.current_day = self.current_time.strftime('%A')
+
+        # Schedule for the current day with random events and noise applied
+        self.schedule = self.set_schedule()
 
         # Heating and Cooling meters
         self.heating_meter = 0.0
@@ -101,7 +104,63 @@ class SmartHomeTempControlEnv(gym.Env):
             self.set_schedule()
 
     def set_schedule(self):
-        pass
+        daily_schedule = self.weekly_schedule[self.current_day]
+
+        # Apply variance to the schedule
+        for person, schedule in daily_schedule.items():
+            if 'leave' in schedule and 'return' in schedule:
+                leave_time = datetime.strptime(schedule['leave'], '%H:%M')
+                return_time = datetime.strptime(schedule['return'], '%H:%M')
+                variance = schedule['variance']
+
+                # Apply variance
+                leave_time += timedelta(minutes=random.randint(-variance, variance))
+                return_time += timedelta(minutes=random.randint(-variance, variance))
+
+                # Update the schedule
+                daily_schedule[person]['leave'] = leave_time.strftime('%H:%M')
+                daily_schedule[person]['return'] = return_time.strftime('%H:%M')
+
+            # Check for random events
+            # if random.random() < self.random_event_chance:
+            if person == 'father':
+                random_event_pick = random.random()
+                starting_chance = 0
+
+                for event, chance in self.random_event.items():
+                    if random_event_pick < starting_chance + chance:
+                        print(f"Random event occured {event} for {person}.")
+                        self.apply_random_event(event, person, daily_schedule)
+                        break
+                    starting_chance += chance
+
+        return daily_schedule
+
+    def apply_random_event(self, event, person, daily_schedule):
+        if event == 'sick-day':
+            daily_schedule[person]['at_home'] = True
+        elif event == 'vacation':
+            daily_schedule[person]['at_home'] = True
+        elif event == 'early-return':
+            return_time = self.parse_time(daily_schedule[person]['return'])
+            return_time -= timedelta(minutes=random.randint(0, self.random_event_max_duration))
+            daily_schedule[person]['return'] = return_time.strftime('%H:%M')
+        elif event == 'late-return':
+            return_time = self.parse_time(daily_schedule[person]['return'])
+            return_time += timedelta(minutes=random.randint(0, self.random_event_max_duration))
+            daily_schedule[person]['return'] = return_time.strftime('%H:%M')
+        elif event == 'early-leave':
+            leave_time = self.parse_time(daily_schedule[person]['leave'])
+            leave_time -= timedelta(minutes=random.randint(0, self.random_event_max_duration))
+            daily_schedule[person]['leave'] = leave_time.strftime('%H:%M')
+        elif event == 'late-leave':
+            leave_time = self.parse_time(daily_schedule[person]['leave'])
+            leave_time += timedelta(minutes=random.randint(0, self.random_event_max_duration))
+            daily_schedule[person]['leave'] = leave_time.strftime('%H:%M')
+        elif event == 'out-of-town':
+            daily_schedule[person]['at_home'] = False
+        elif event == 'holiday':
+            daily_schedule[person]['at_home'] = False
 
     def execute_action(self, action):
         if action == 0:  # Heat up
@@ -129,3 +188,6 @@ class SmartHomeTempControlEnv(gym.Env):
     def parse_weekday_return_time(self, return_times):
         weekday = self.current_time.strftime('%a')
         return self.parse_time(return_times.get(weekday, '15:00'))  # default return time if not specified
+
+simulation = SmartHomeTempControlEnv()
+print(simulation.schedule)
